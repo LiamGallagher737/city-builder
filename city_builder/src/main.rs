7da0c357ed71014af1d19455d402c25d0e079801
Liam Gallagher <ljpgallagher@xtra.co.nz>
2022-05-24 14:58:00 +1200

    testing in robotics

--- city_builder/src/main.rs
+++ city_builder/src/main.rs
@@ -1,9 +1,13 @@
-mod game;
-
 use bevy::prelude::*;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+
+mod game;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(game::GamePlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .run();
 }
