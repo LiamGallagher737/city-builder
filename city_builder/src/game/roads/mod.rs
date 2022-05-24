1d6854a0ef34c272622e76ec2a6a240094cc665f
LiamGallagher737 <86995276+LiamGallagher737@users.noreply.github.com>
2022-05-24 19:16:22 +1200

    basic scene

--- city_builder/src/game/roads/mod.rs
+++ city_builder/src/game/roads/mod.rs
@@ -6,5 +6,6 @@ pub struct RoadsPlugin;
 impl Plugin for RoadsPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(road_network::road_network_startup_system);
+        
     }
 }
--- city_builder/src/main.rs
+++ city_builder/src/main.rs
@@ -1,13 +1,50 @@
 use bevy::prelude::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::window::PresentMode;
 
 mod game;
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            present_mode: PresentMode::Immediate,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugins(game::GamePlugins)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_startup_system(scene_setup)
         .run();
+}
+
+fn scene_setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+
+    // Camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // Point Light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    // Ground Plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
 }
