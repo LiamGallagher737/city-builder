fce05491952725e149d733c8f66a57bd66cdd1bb
LiamGallagher737 <86995276+LiamGallagher737@users.noreply.github.com>
2022-05-25 22:10:00 +1200

    wtf is going on

--- city_builder/src/game/roads/components.rs
+++ city_builder/src/game/roads/components.rs
@@ -1,11 +1,56 @@
+use std::sync::{RwLock, Arc};
+
+use bevy::{utils::hashbrown::HashSet, prelude::Vec3};
+
 pub struct RoadNetwork {
-    // roads: HashMap< >
+    pub roads: Arc<RwLock<HashSet<Road>>>,
 }
 
 impl RoadNetwork {
     pub fn new() -> Self {
         Self {
-
+            roads: Arc::new(RwLock::new(HashSet::new())),
         }
     }
+}
+
+pub struct Road {
+    pub nodes: Vec<Node>,
+    pub intersection_start: Option<Arc<RwLock<Intersection>>>,
+    pub intersection_end: Option<Arc<RwLock<Intersection>>>,
+}
+
+impl Road {
+    pub fn new() -> Self {
+        Self { 
+            nodes: Vec::new(), 
+            intersection_start: Option::None, 
+            intersection_end: Option::None,
+        }
+    }
+}
+
+pub struct Node {
+    pub position: Vec3,
+    pub control_a: Vec3,
+    pub control_b: Vec3,
+}
+
+pub struct Intersection {
+    pub position: Vec3,
+    pub roads: HashSet<(Arc<RwLock<Road>>, RoadCap)>
+}
+
+impl Intersection {
+    pub fn new() -> Self {
+        Self {
+            position: Vec3::default(),
+            roads: HashSet::new(),
+        }
+    }
+}
+
+pub enum RoadCap {
+    Start,
+    End,
 }
--- city_builder/src/game/roads/mod.rs
+++ city_builder/src/game/roads/mod.rs
@@ -7,6 +7,5 @@ pub struct RoadsPlugin;
 impl Plugin for RoadsPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(road_network::road_network_startup_system);
-        
     }
 }
