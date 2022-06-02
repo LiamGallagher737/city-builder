ff32c94c6f7b8caa7c7f64daa27709ba0bb58d35
LiamGallagher737 <86995276+LiamGallagher737@users.noreply.github.com>
2022-06-02 22:46:53 +1200

    next up: check for intersecctions

--- city_builder/src/game/roads/components.rs
+++ city_builder/src/game/roads/components.rs
@@ -1,5 +1,5 @@
-use std::sync::{RwLock, Arc};
-use bevy::{utils::hashbrown::HashSet, prelude::{Vec3, Component}};
+use std::{sync::{RwLock, Arc}, f32::consts::PI};
+use bevy::{utils::hashbrown::HashSet, prelude::{Vec3, Quat, Component}};
 
 pub struct RoadNetwork {
     pub roads: HashSet<Arc<RwLock<Road>>>,
@@ -29,24 +29,36 @@ impl Road {
     }
 }
 
+#[derive(Debug)]
 pub struct Node {
     pub position: Vec3,
     pub control_a: Vec3,
     pub control_b: Vec3,
 }
 
+impl Node {
+    // Creates a new node as chosen position and generates 2 control nodes based on rotation
+    pub fn new(position: Vec3, rotation: Quat) -> Self {
+        let sinp = 2.0 * (rotation.w * rotation.y - rotation.z * rotation.x);
+        let euler_y: f32;
+        if sinp.abs() >= 1.0 {
+            euler_y = PI / 2.0 * sinp.sin();
+        } else {
+            euler_y = sinp.asin();
+        }
+        let forward = Vec3::new(euler_y.cos(), 0.0, euler_y.sin());
+        
+        Self {
+            position: position,
+            control_a: position + forward * 1.0,
+            control_b: position - forward * 1.0,
+        }
+    }
+}
+
 pub struct Intersection {
     pub position: Vec3,
     pub roads: HashSet<(Arc<RwLock<Road>>, RoadCap)>
-}
-
-impl Intersection {
-    pub fn new() -> Self {
-        Self {
-            position: Vec3::default(),
-            roads: HashSet::<(Arc<RwLock<Road>>, RoadCap)>::new(),
-        }
-    }
 }
 
 pub enum RoadCap {
@@ -56,13 +68,15 @@ pub enum RoadCap {
 
 #[derive(Component)]
 pub struct RoadCreator {
-    pub current_road: Option<Arc<RwLock<Road>>>,
+    pub current_road_nodes: Option<Vec<Node>>,
+    pub start_intersection: Option<Arc<RwLock<Intersection>>>,
 }
 
 impl Default for RoadCreator {
     fn default() -> Self {
         Self {
-            current_road: None
+            current_road_nodes: None,
+            start_intersection: None,
         }
     }
 }
--- city_builder/src/game/roads/road_creation.rs
+++ city_builder/src/game/roads/road_creation.rs
@@ -1,23 +1,38 @@
 use std::sync::{Arc, RwLock};
 
-use bevy::{prelude::*, utils::hashbrown::HashSet};
+use bevy::{utils::hashbrown::HashSet, prelude::{ResMut, Query, Transform, Vec3}};
 
 use super::components::*;
 
+const NEW_NODE_DISTANCE_SQ: f32 = 10.0 * 10.0;
+const JOIN_INTERSECTION_DISTANCE_SQ: f32 = 2.0 * 2.0;
+
 pub fn road_creation_system(
-    mut road_network: ResMut<RoadNetwork>,
-    mut query: Query<(&mut RoadCreator, &Transform)>
+    mut _road_network: ResMut<RoadNetwork>,
+    mut query: Query<(&mut RoadCreator, &mut Transform)>
 ) {
-    if let Ok((mut road_creator, tf)) = query.get_single_mut() {
-        if road_creator.current_road.is_none() {
-            road_creator.current_road = Some(Arc::new(RwLock::new(Road {
-                nodes: vec![],
-                intersection_start: Some(Arc::new(RwLock::new(Intersection {
-                    position: tf.translation,
-                    roads: HashSet::<(Arc<RwLock<Road>>, RoadCap)>::new()
-                }))),
-                intersection_end: None,
+    if let Ok((mut road_creator, mut tf)) = query.get_single_mut() {
+
+        tf.translation.x += 0.05;
+
+        if road_creator.current_road_nodes.is_none() {
+
+            road_creator.start_intersection = Some(Arc::new(RwLock::new(Intersection {
+                position: tf.translation,
+                roads: HashSet::<(Arc<RwLock<Road>>, RoadCap)>::new(),
             })));
+
+            road_creator.current_road_nodes = Some(vec![Node::new(tf.translation, tf.rotation)]);
+
+            return;
         }
+
+        if road_creator.current_road_nodes.as_ref().unwrap().last().unwrap().position.distance_squared(tf.translation) >= NEW_NODE_DISTANCE_SQ {
+            road_creator.current_road_nodes.as_mut().unwrap().push(Node::new(tf.translation, tf.rotation));
+
+            // println!("{:#?} \n\n\n", road_creator.current_road_nodes);
+        }
+
+        // TODO: check for intersection
     }
 }
