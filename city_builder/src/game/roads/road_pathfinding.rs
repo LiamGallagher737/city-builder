b225b808da2867aa708c215d9186a0bfda9f9be7
LiamGallagher737 <86995276+LiamGallagher737@users.noreply.github.com>
2022-06-22 23:07:21 +1200

    aaaaaaahhhhh

--- city_builder/src/game/roads/road_pathfinding.rs
+++ city_builder/src/game/roads/road_pathfinding.rs
@@ -5,15 +5,15 @@ impl RoadNetwork {
     pub fn calculate_path(self: &Self, start: Address, end: Address) -> Option<Vec<RoadKey>> {
         // dijkstra's algorithm
 
-
+        // https://youtu.be/EFg3u_E6eHU
 
         None
     }
 }
 
 impl Road {
-    pub fn calculate_dijkstra_value(self: &Self) -> i32 {
-        // if let Some(value) = 
+    pub fn calculate_dijkstra_value(self: &Self) -> u32 {
+        // cant b negative
         5
     }
 }
